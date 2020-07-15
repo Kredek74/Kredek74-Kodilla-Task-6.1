31572c617c61daa18804f1b15746dbea97275291
Kredek74 <Krdek74@protonmail.com>
2020-07-16 01:21:58 +0200

    Task 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        int addResult = 12;
+        int subtractResult = 8;
+
+        if (addResult == calculator.additionAtoB(7, 5)) {
+            System.out.println("Addition works");
+        } else {
+            System.out.println("Error of method add");
+        }
+
+        if (subtractResult == calculator.substractionAfromB(14, 6)) {
+            System.out.println("Substraction works");
+        } else System.out.println("Error of method subtract");
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int additionAtoB(int a, int b){
+        System.out.println("The result equals: " + (a+b));
+        return a+b;
+    }
+    public int substractionAfromB(int a, int b){
+        System.out.println("The result equals: " + (a-b));
+        return a-b;
+    }
+
+}
